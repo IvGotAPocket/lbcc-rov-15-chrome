3fc39de1bea5e946d0593d997fa83a2b2a974504
Rob Mac <maccelerated@gmail.com>
2015-03-13 03:31:07 -0700

    init2

--- js/app/app.js
+++
@@ -1,5 +0,0 @@
-angular.module('WaterBearApp', [
-	'WaterBearApp.Controllers',
-	'WaterBearApp.Directives',
-	'WaterBearApp.Services'
-]);
--- js/background.js
+++ js/background.js
@@ -1,12 +1,12 @@
 chrome.app.runtime.onLaunched.addListener(function() {
 	chrome.app.window.create('window.html', {
-		'bounds': {
-			'width': 500,
-			'height': 500
+		bounds: {
+			width	: 800,
+			height	: 600
 		},
 		//minWidth: 500,
     	//minHeight: 900,
-    	//frame: 'none'
+    	frame: 'none'
 	}, function (window) {
 		//window.maximize();
 	});
